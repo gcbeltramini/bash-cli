730ee379c3346706e868c57b4df54033fe09f1b3
Guilherme Beltramini <gcbeltramini@gmail.com>
2026-03-17 19:43:54 -0300

    Split word in the "=" sign, and treat slashes

--- core/cli_root/autocomplete.zsh
+++ core/cli_root/autocomplete.zsh
@@ -59,7 +59,8 @@ _mycli_completions() {
     # words[1]=mycli, words[2]=<cmd1>, words[3]=<cmd2> are already considered above
     local args_with_description=$all_args_with_description
     for word in "${(@)words[4,-1]}"; do # same as: "${words[@]:3}" or ${words[4,-1]} (unquoted to allow word splitting)
-      args_with_description=$(echo "$args_with_description" | sed "/^$word:/d")
+      local option_name=${word%%=*}
+      args_with_description=$(echo "$args_with_description" | awk -F':' -v arg="$option_name" '$1 != arg')
     done
 
     if [[ -z "$args_with_description" ]]; then
